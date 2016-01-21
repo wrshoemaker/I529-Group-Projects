37e3147550b82a233d5c5a91b0cda357c9ffb6c6
Will Shoemaker <shoemakah@gmail.com>
2016-01-21 16:34:50 -0500

    File strcture alteration, minor

--- Project1/Python/ECgnfinder_WRS.py
+++ Project1/Python/ECgnfinder_WRS.py
@@ -1,21 +1,27 @@
+import os
+
+
+mydir = os.path.dirname(os.path.realpath(__file__))
+mydir = str(mydir[:-6]) + 'data/'
+
 class fasta:
 
     def __init__(self, fileFASTA):
         self.fileFASTA = fileFASTA
 
-    def readFASTA(fileFASTA):
+    def readFASTA(self):
         '''Checks for fasta by file extension'''
-        file_lower = fileFASTA.lower()
+        file_lower = self.fileFASTA.lower()
         '''Check for three most common fasta file extensions'''
         if file_lower.endswith('.txt') or file_lower.endswith('.fa') or \
         file_lower.endswith('.fasta') or file_lower.endswith('.fna'):
-            with open(fileFASTA, "r") as f:
+            with open(self.fileFASTA, "r") as f:
                 return ParseFASTA(f)
 
-    def ParseFASTA(fileFasta):
+    def ParseFASTA(self, fileFASTA):
         '''Gets the sequence name and sequence from a FASTA formatted file'''
         fasta_list=[]
-        for line in fileFasta:
+        for line in fileFASTA:
             if line[0] == '>':
                 try:
                     fasta_list.append(current_dna)
@@ -28,3 +34,5 @@ class fasta:
                 current_dna[1] += "".join(line.split())
         fasta_list.append(current_dna)
         return fasta_list
+
+print mydir
