d4968b85815e4f8a51fed9f1a9249c7f74b17302
Xiangyu Yao <xianyao@indiana.edu>
2016-03-04 23:44:34 -0500

    add general parsing of Newick

--- Project3/Python/test.py
+++ Project3/Python/test.py
@@ -2,65 +2,60 @@
 
 from itertools import groupby
 
-class BinaryTree:
-	def __init__(self,newick):
-                '''read the newick string and return the root of the tree'''
-                self.newick = newick
-	        self.left = None
+
+class Node:
+	def __init__(self,length):	
+		self.length = length#length to its parent
+		self.left = None
 		self.right = None
-		self.value = [0.25,0.25,0.25,0.25]#the background probability
-		self.length = 0# initial length to parent is 0
-	def getLeftChild(self):
-		return self.left
-	def getRightChild(self):
-		return self.right
-	def setNodeLength(self,length):#this is the length betwenn this node and its parent
-		self.length = length
-	def setNodeValue(self,value):
-		self.value = value
-        def insertLeft(self,newNode):
-                self.left = newNode
-        def insertRight(self,newNode):
-                self.right = newNode
-            
-        def break_tree(self):
-            	tmp = self.newick
-		if ',' not in tmp:
-			return False
+		self.value = None
+	
+
+class BinaryTree:
+	def __init__(self):
+		self.root = None
+
+class Newick:
+	def __init__(self,newick):
+		self.newick = newick
+	def divide(self):
+		if ',' not in self.newick:
+			length = float(self.newick.split(':')[1])
+			tmp = Node(length)
+			return tmp
 		else:
-			tab = [''.join(g) for k,g in groupby(tmp)]
-			number_left = len(tab[0]) - 1# number of left parenthesis
+			tab = [''.join(g) for k,g in groupby(self.newick)]
+			number_left = len(tab[0]) - 1
 			number_right = 0
-			for i in range(len(tmp)):
-				if  tmp[i] == ')':
+			total = len(self.newick)
+
+			for i in range(total):
+				if  self.newick[i] == ')':
 					number_right += 1
 				if number_right == number_left:
-					for j in range(i+1,len(tmp)):
-						if tmp[j] == ',':
+					for j in range(i+1,total):
+						if self.newick[j] == ',':
 							break
 					break
-			self.left = BinaryTree(tmp[:j] + ')')
-			self.right= BinaryTree('(' + tmp[j+1:])
-			return True
-    
-#build the tree given Newick tree
-def build_tree(Newick):
-	root = BinaryTree(Newick)
-	if not root.break_tree():#ony one node exists in this graph
-		return 
-	else:
-		root.break_tree()
-		left_sub = root.getLeftChild()
-		right_sub = root.getRightChild()
-		build_tree(left_sub.newick)
-		build_tree(right_sub.newick)
-		return
 
+			k = self.newick.rfind(':')
+			length = float(self.newick[k+1:])
+			ancestor = Node(length)
+			tmp_left = self.newick[1:j]
+			tmp_right = self.newick[j+1:k-1]
+			left_sub = Newick(tmp_left)#left sub tree
+			right_sub = Newick(tmp_right)#right sub tree
+		
+			ancestor.left = left_sub.divide()
+			ancestor.right = right_sub.divide()
+			return ancestor
 
 ####### use the simple test Newick string to check result		
 test = '((Human:0.3,Chimpanzee:0.2):0.1,Gorilla:0.3)'   
-t = BinaryTree(test)
-print(t.newick)
-t.break_tree()   
-print(t.getRightChild().newick)
-build_tree(test)
+
+test_newick = Newick(test + ':0.0')
+tree = BinaryTree()
+tree.root = test_newick.divide()
+print(tree.root.left.right.length)
+
+
