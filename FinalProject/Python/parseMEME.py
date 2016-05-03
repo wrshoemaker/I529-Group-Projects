f0c18a0eb0a89abbfe6623210124315b66594526
Will Shoemaker <shoemakah@gmail.com>
2016-05-03 13:35:58 -0400

    new figs

--- FinalProject/Python/parseMEME.py
+++ FinalProject/Python/parseMEME.py
@@ -1,13 +1,76 @@
 from __future__ import division
 import pandas as pd
-import os
+import numpy as np
+import os, argparse, random, math
+from collections import Counter
+from itertools import product
 
 mydir = os.path.expanduser("~/github/I529-Group-Projects/FinalProject/")
 IN = mydir + 'data/Meme_final/meme.txt'
 
+class classFASTA:
 
-# motif dict structure
-# motif# { sequenceName {count, (start position(s))  } }
+    def __init__(self, fileFASTA):
+        self.fileFASTA = fileFASTA
+
+    def readFASTA(self):
+        '''Checks for fasta by file extension'''
+        file_lower = self.fileFASTA.lower()
+        '''Check for three most common fasta file extensions'''
+        if file_lower.endswith('.txt') or file_lower.endswith('.fa') or \
+        file_lower.endswith('.fasta') or file_lower.endswith('.fna'):
+            with open(self.fileFASTA, "r") as f:
+                return self.ParseFASTA(f)
+        else:
+            print "Not in FASTA format."
+
+    def ParseFASTA(self, fileFASTA):
+        '''Gets the sequence name and sequence from a FASTA formatted file'''
+        fasta_list=[]
+        for line in fileFASTA:
+            if line[0] == '>':
+                try:
+                    fasta_list.append(current_dna)
+            	#pass if an error comes up
+                except UnboundLocalError:
+                    print "Inproper file format."
+                    pass
+                current_dna = [line.lstrip('>').rstrip('\n'),'']
+            else:
+                current_dna[1] += "".join(line.split())
+        fasta_list.append(current_dna)
+        '''Returns fasa as nested list, containing line identifier \
+            and sequence'''
+        return fasta_list
+
+def gc(n):
+    count = 0
+    length = len(n)
+    for i in n:
+        if i == "C" or i == "G":
+            count += 1
+    GCrel = count / length
+    return round(GCrel, 3)
+
+def promotorToFeatures():
+    fasta = mydir + 'data/DREAM6_ExPred_Promoters.fasta'
+    class_test = classFASTA(fasta)
+    OUT =  open(mydir+'data/DREAM6_ExPred_Promoters_Features.txt', 'w')
+    for x in class_test.readFASTA():
+        lenSeq = np.log10(len(x[1]))
+        GCcontent = gc(x[1])
+        print>> OUT, x[0], lenSeq, GCcontent
+    OUT.close()
+
+def mergeFeatureSet():
+    feature1 = pd.read_csv(mydir + 'data/DREAM6_ExPred_Promoters_Features.txt', \
+        sep = ' ', header = None, names = ["Sequence", "Length(log10)", "GC"])
+    feature2 = pd.read_csv(mydir + 'data/DREAM6_ExPred_PromoterActivities.txt', \
+        sep = '\t', header = None, names = ["Sequence", "Expression"])
+    featuresMerged = pd.merge(feature1, feature2, left_on = 'Sequence', right_on = 'Sequence')
+    featuresMerged.to_csv(path_or_buf = mydir + 'data/DREAM6_ExPred_Promoters_Features_Activitis.txt', \
+        sep = '\t', index=False)
+
 
 def getMotifCounts():
     lengthDict = {}
@@ -35,6 +98,8 @@ def getMotifCounts():
         if i > 150 and len(line) == 6:
             motifDict[motifNum][line[0]] += 1
     return motifDict
+
+mergeFeatureSet()
 motifDictTest = getMotifCounts()
 motifDictTestPandas =  pd.DataFrame.from_dict(motifDictTest)
 motifDictTestPandas['Sequence'] = motifDictTestPandas.index
--- FinalProject/Python/test.py
+++ FinalProject/Python/test.py
@@ -6,78 +6,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, cross_val_score, LeaveOneOut,cross_val_predict
 from sklearn.linear_model import LinearRegression, Lasso, ElasticNet
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import cross_validation
 sns.set(style='whitegrid', context='notebook')
 
-mydir = os.path.dirname(os.path.realpath(__file__))
-mydir = str(mydir[:-6]) + 'data/'
+mydir = os.path.expanduser("~/github/I529-Group-Projects/FinalProject/")
+IN = mydir + 'data/Meme_final/meme.txt'
 
-class classFASTA:
 
-    def __init__(self, fileFASTA):
-        self.fileFASTA = fileFASTA
-
-    def readFASTA(self):
-        '''Checks for fasta by file extension'''
-        file_lower = self.fileFASTA.lower()
-        '''Check for three most common fasta file extensions'''
-        if file_lower.endswith('.txt') or file_lower.endswith('.fa') or \
-        file_lower.endswith('.fasta') or file_lower.endswith('.fna'):
-            with open(self.fileFASTA, "r") as f:
-                return self.ParseFASTA(f)
-        else:
-            print "Not in FASTA format."
-
-    def ParseFASTA(self, fileFASTA):
-        '''Gets the sequence name and sequence from a FASTA formatted file'''
-        fasta_list=[]
-        for line in fileFASTA:
-            if line[0] == '>':
-                try:
-                    fasta_list.append(current_dna)
-            	#pass if an error comes up
-                except UnboundLocalError:
-                    #print "Inproper file format."
-                    pass
-                current_dna = [line.lstrip('>').rstrip('\n'),'']
-            else:
-                current_dna[1] += "".join(line.split())
-        fasta_list.append(current_dna)
-        '''Returns fasa as nested list, containing line identifier \
-            and sequence'''
-        return fasta_list
-
-def gc(n):
-    count = 0
-    length = len(n)
-    for i in n:
-        if i == "C" or i == "G":
-            count += 1
-    GCrel = count / length
-    return round(GCrel, 3)
-
-def promotorToFeatures():
-    fasta = mydir + 'DREAM6_ExPred_Promoters.fasta'
-    class_test = classFASTA(fasta)
-    OUT =  open(mydir+'DREAM6_ExPred_Promoters_Features.txt', 'w')
-    # print sequence name, length, and GC-content to file
-    for x in class_test.readFASTA():
-        lenSeq = np.log10(len(x[1]))
-        GCcontent = gc(x[1])
-        print>> OUT, x[0], lenSeq, GCcontent
-    OUT.close()
-
-def mergeFeatureSet():
-    feature1 = pd.read_csv(mydir + 'DREAM6_ExPred_Promoters_Features.txt', \
-        sep = ' ', header = None, names = ["Sequence", "Length(log10)", "GC"])
-    feature2 = pd.read_csv(mydir + 'DREAM6_ExPred_PromoterActivities.txt', \
-        sep = '\t', header = None, names = ["Sequence", "Expression"])
-    featuresMerged = pd.merge(feature1, feature2, left_on = 'Sequence', right_on = 'Sequence')
-    featuresMerged.to_csv(path_or_buf = mydir + 'DREAM6_ExPred_Promoters_Features_Activitis.txt', \
-        sep = '\t', index=False)
 
 def plotResiduals(y_train, y_test, y_train_pred, y_test_pred, filename = 'residuals'):
     plt.scatter(y_train_pred, y_train_pred - y_train, \
@@ -89,18 +27,25 @@ def plotResiduals(y_train, y_test, y_train_pred, y_test_pred, filename = 'residu
     plt.legend(loc = 'upper left')
     plt.hlines(y = 0, xmin = -10, xmax = 50, lw =2, color = 'red')
     plt.xlim([-5, 5])
-    plt.savefig(str(mydir[:-6]) + '/figs/' +  filename + "png")
+    plt.savefig(str(mydir) + 'figs/' +  filename + ".png")
 
-df = pd.read_csv(mydir + 'DREAM6_ExPred_Promoters_Features_Activitis.txt',\
+def pairPlot(df):
+    cols = ['Length(log10)','GC','Expression', '1', '2', '3', '4', '5']
+    sns_plot = sns.pairplot(df[cols], size=2.5)
+    sns_plot.savefig(str(mydir) + 'figs/' + "output.png")
+
+
+
+df = pd.read_csv(mydir + 'data/MEME_dataframe.txt',\
     sep = '\t')
-#cols = ['Length(log10)','GC','Expression']
-#sns_plot = sns.pairplot(IN[cols], size=2.5)
-#sns_plot.savefig(str(mydir[:-6]) + '/figs/' + "output.png")
 
 X = df.iloc[:, 1:-1].values
 y = df['Expression'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, \
     random_state = 0)
+
+
+
 slr = LinearRegression()
 slr.fit(X_train, y_train)
 y_train_pred = slr.predict(X_train)
@@ -112,6 +57,21 @@ print('MSE train: %.3f, test: %.3f' % (
 print('R^2 train: %.3f, test: %.3f' % (
         r2_score(y_train, y_train_pred),
         r2_score(y_test, y_test_pred)) )
+
+
+# cross validated
+
+
+# cross_val_predict returns an array of the same size as `y` where each entry
+# is a prediction obtained by cross validated:
+predicted = cross_val_predict(slr, X, y, cv=20)
+fig, ax = plt.subplots()
+ax.scatter(y, predicted)
+ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+plt.savefig(str(mydir) + 'figs/' +  'testCV' + ".png")
+
 
 # Lets try looking at the data with LASSO and Elastic Net
 EN = ElasticNet(alpha = 1.0, l1_ratio = 0.5)
@@ -130,3 +90,5 @@ scores2 = cross_validation.cross_val_score( \
 print "Lasso"
 print scores2.mean()
 print scores2.std()
+
+plotResiduals(y_train, y_test, y_train_pred,  y_test_pred, filename = 'residuals1')
